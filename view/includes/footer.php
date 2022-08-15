5ae797a5a5f4587a2010a770ddc583ddb67f9884
Falcao1303 <lucasjesus22@hotmail.com>
2022-08-15 00:04:01 -0300

    ajustes novo repositorio

---
+++ view/includes/footer.php
@@ -0,0 +1,14 @@
+<style>
+    footer{
+    background-color: #03022e;
+    color: #fff;
+    margin-top: 100px;
+    padding: 5px 0;
+    width: 100%;
+}  
+</style>
+        
+          
+          <footer>
+
+            </footer>
